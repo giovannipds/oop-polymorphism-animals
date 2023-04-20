258b068be80e3a6db84047303f791b27d30262d3
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-04-20 20:48:17 -0300

    javali

---
+++ src/animais/Animais.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package animais;
+
+/**
+ *
+ * @author giovannipds
+ */
+public class Animais {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Animal javali = new Javali();
+        javali.movimentar();
+        javali.comunicar();
+        
+        System.out.println(javali instanceof Animal);
+    }
+    
+}
---
+++ src/animais/Javali.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animais;
+
+/**
+ *
+ * @author giovannipds
+ */
+public class Javali extends Animal{
+
+    @Override
+    public void comunicar() {
+        System.out.println("Grunir");
+    }
+
+    @Override
+    public void movimentar() {
+        System.out.println("Caminhar");
+    }
+    
+}
